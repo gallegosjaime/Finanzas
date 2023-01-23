bc7dd1708e6e23443096bf657f2d3619f4e8e543
gallegosjaime <104153916+gallegosjaime@users.noreply.github.com>
2023-01-23 17:08:30 -0600

    uploaded

---
+++ Finanzas/Finanzas/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Finanzas.ViewModels;
+using Finanzas.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Finanzas
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Finanzas/Finanzas/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Finanzas.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Finanzas.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
