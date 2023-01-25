baacf26f7decd308c9d284db51950acba32b64a8
gallegosjaime <104153916+gallegosjaime@users.noreply.github.com>
2023-01-25 12:56:45 -0600

    uploaded

--- Finanzas/Finanzas/App.xaml.cs
+++ Finanzas/Finanzas/App.xaml.cs
@@ -12,7 +12,6 @@ namespace Finanzas
         public App()
         {
             InitializeComponent();
-
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
